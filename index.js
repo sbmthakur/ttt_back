057e103d0995e30a5fe2d4ef693564a4f8bb99c2
shubham thakur <sbmthakur@gmail.com>
2017-12-31 18:13:43 +0530

    Remove response header for a while

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/:limit", (req, res) => {
   const limit = req.params.limit;
   res.setHeader("Content-Type", "application/json");
   getdata(limit, (err, result) => {
-    res.header("Access-Control-Allow-Origin", "*.herokuapp.com");
+    //res.header("Access-Control-Allow-Origin", "*.herokuapp.com");
     res.end(JSON.stringify(result));
   });
 });
