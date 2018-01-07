bd5363fbb3d0f180dcd22932c892a4854779edd5
shubham thakur <sbmthakur@gmail.com>
2018-01-08 00:28:16 +0530

    Add timeoutHandler and handle error

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ const app = express();
 
 app.get("/:limit", (req, res) => {
 
+  /* limit is the total number of words
+   * that will be returned with a succ-
+   * essful response
+   */
   const limit = req.params.limit;
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "https://ttt-form.herokuapp.com");
+
   getdata(limit, (err, result) => {
-    res.header("Access-Control-Allow-Origin", "https://ttt-form.herokuapp.com");
-    res.end(JSON.stringify(result));
+    if (err) {
+      res.status(500).end(err);
+    }
+    else {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(result));
+    }
   });
+
+  function timeoutHandler() {
+    return res.status(504).end("Server Timeout");
+  }
+
+  /* Timeout response after 10 seconds */
+  res.setTimeout(10000, timeoutHandler);
 });
 
 app.listen(process.env.PORT || 3000);
